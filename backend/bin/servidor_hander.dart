b7919ca3ad092638ee22672d7309472b1501b5a0
ErickMaier <erick1maier@gmail.com>
2024-06-04 19:29:31 -0300

    adicionar primeira api

--- backend/bin/servidor_hander.dart
+++ backend/bin/servidor_hander.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf.dart';
 
@@ -7,6 +9,27 @@ class ServidorHandler {
 
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira Rota');
+    });
+
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok("Ola Mundo $usuario");
+    });
+
+    router.get('/query', (Request req) {
+      String? nome = req.url.queryParameters['nome'];
+      String? idade = req.url.queryParameters['idade'];
+      return Response.ok("Query eh: $nome e $idade");
+    });
+
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+
+      Map json = jsonDecode(result);
+      if (json['usuario'] == 'admin' && json['senha'] == '123') {
+        return Response.ok("Bem vindo admin!");
+      } else {
+        return Response.forbidden("Acesso negado");
+      }
     });
 
     return router;
