c4d46ab2a3f9113d4074328d9b6e9dc1217ea1b2
ErickMaier <erick1maier@gmail.com>
2024-07-01 09:39:48 -0300

    refactor task_card to stateless

--- frontend/organize_app/lib/custom_widgets/task_card.dart
+++ frontend/organize_app/lib/custom_widgets/task_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class TaskCard extends StatefulWidget {
+class TaskCard extends StatelessWidget {
   final String title;
   final String subtitle;
   final int countTasks;
@@ -9,55 +9,50 @@ class TaskCard extends StatefulWidget {
       required this.title,
       required this.subtitle,
       required this.countTasks});
-
-  @override
-  State<TaskCard> createState() => _TaskCardState();
-}
-
-class _TaskCardState extends State<TaskCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      width: double.infinity,
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(15)),
-          border: Border.all(width: 2, color: Colors.black)),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.title,
-                  style: const TextStyle(
-                      fontSize: 19, fontWeight: FontWeight.w600),
-                ),
-                Text(
-                  widget.subtitle,
-                  style: const TextStyle(fontSize: 14),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: [
-                    const Icon(Icons.check_circle),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text("${widget.countTasks} Tasks")
-                  ],
-                )
-              ],
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+            border: Border.all(width: 2, color: Colors.black)),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 19, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    subtitle,
+                    style: const TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.check_circle),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text("$countTasks Tasks")
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          const Text(
-            "Foto",
-            style: TextStyle(fontSize: 17),
-          )
-        ],
+            const Text(
+              "Foto",
+              style: TextStyle(fontSize: 17),
+            )
+          ],
+        ),
       ),
     );
   }
